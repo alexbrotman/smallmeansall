713b18275ab170c9878c3dcf96ae8e944f28e265
Alex Brotman <alexbrotman@gmail.com>
2020-04-30 22:16:32 -0500

    Removes test code

--- code-generator/scripts.js
+++ code-generator/scripts.js
@@ -1,33 +1,6 @@
 var settings = {
   prefix: "S-",
-  baseConversion: 36,
-  // dictionary: {
-  //   "0": "2",
-  //   "1": "3",
-  //   "2": "4",
-  //   "3": "5",
-  //   "4": "6",
-  //   "5": "7",
-  //   "6": "8",
-  //   "7": "9",
-  //   "8": "B",
-  //   "9": "C",
-  //   A: "D",
-  //   B: "F",
-  //   C: "G",
-  //   D: "H",
-  //   E: "J",
-  //   F: "K",
-  //   G: "M",
-  //   H: "N",
-  //   I: "P",
-  //   J: "Q",
-  //   K: "R",
-  //   L: "S",
-  //   M: "V",
-  //   N: "X",
-  //   O: "Z",
-  // },
+  baseConversion: 36
 };
 
 function buildTimestamp() {
@@ -40,11 +13,6 @@ function buildTimestamp() {
 function buildCode(timestamp, rep) {
   timestamp += rep;
   var encoded = timestamp.toString(settings.baseConversion).toUpperCase();
-  // for (var i = 0; i < encoded.length; i++) {
-  //   var oldChar = encoded[i];
-  //   var newChar = settings.dictionary[oldChar];
-  //   encoded = encoded.replace(oldChar, newChar);
-  // }
   return settings.prefix + encoded;
 }
 
